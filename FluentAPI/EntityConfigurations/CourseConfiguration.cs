ddfecb6221e839e661ef97717508db0a8983878d
HenriqueSD <hsd.foz@gmail.com>
2019-05-29 20:55:56 -0300

    Organized Fluent API Configurations

---
+++ FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            // *the comments below are just sugestions;
+
+            // change the name of the table;
+            ToTable("tbl_Course");
+
+            // override primary keys;
+            HasKey(c => c.Id);
+
+            // property configurations (sort then alphabetically);
+
+            Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            // relationships (sort then alphabetically);
+            HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(c => c.Tags)
+            .WithMany(t => t.Courses)
+            .Map(m =>
+            {
+                m.ToTable("CourseTags");
+                m.MapLeftKey("CourseId");
+                m.MapRightKey("TagId");
+            });
+        }
+    }
+}
--- FluentAPI/PlutoContext.cs
+++ FluentAPI/PlutoContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using FluentAPI.EntityConfigurations;
+using System.Data.Entity;
 
 namespace FluentAPI
 {
@@ -15,40 +16,7 @@ namespace FluentAPI
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>()
-            .Property(c => c.Name)
-            .IsRequired()
-            .HasMaxLength(255);
-
-            modelBuilder.Entity<Course>()
-              .Property(c => c.Description)
-              .IsRequired()
-              .HasMaxLength(2000);
-
-            // To configure the relationship between courses and authors, it doesn't matter which end
-            // of the relationship you start;
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Author)
-                .WithMany(a => a.Courses)
-                .HasForeignKey(c => c.AuthorId)
-                .WillCascadeOnDelete(false);
-
-            // we can start from Course or from Tag, doesn't matter;
-            modelBuilder.Entity<Course>()
-                .HasMany(c => c.Tags)
-                .WithMany(t => t.Courses)
-                .Map(m =>
-                {
-                    m.ToTable("CourseTags");
-                    m.MapLeftKey("CourseId");
-                    m.MapRightKey("TagId");
-                 });
-
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Cover)
-                .WithRequiredPrincipal(c => c.Course);
-
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new CourseConfiguration());
         }
     }
 }
